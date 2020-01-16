d56ad26f3ffcdb8a74ee21997802751d3ac32b77
Bence <56838314+1s3k3b@users.noreply.github.com>
2020-01-16 15:27:49 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -6,7 +6,7 @@ const generate = (min = 1, max = 1000, operators = ["+", "-", "*", "/", "%"]) =>
     for (const op of operators) {
         for (let i = min; i <= max; i++) {
             for (let j = min; j <= max; j++) {
-                output += `if (o === ${op} && a === ${i} && b === ${j}) return ${i} ${op} ${j};\n\n    `;
+                output += `if (o === "${op}" && a === ${i} && b === ${j}) return ${i} ${op} ${j};\n    `;
             }
         }
     }
