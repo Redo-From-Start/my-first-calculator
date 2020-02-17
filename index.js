a33a6835ba8129aa668d55eb5858e9ca69f7d7fc
RedoFromStart <55228370+Redo-From-Start@users.noreply.github.com>
2020-02-17 08:34:15 +0000

    more efficient

---
+++ index.js
@@ -0,0 +1,16 @@
+// slightly more efiecent :)
+
+function calculate(a, b, o){
+    if (o ===  "+"){
+        return a + b;
+    }
+    if (o ===  "-"){
+        return a - b;
+    }
+    if (o ===  "*"){
+        return a * b;
+    }
+    if (o ===  "/"){
+        return a / b;
+    }
+}
